0a88c9827469678f068c431f57cee04d6d491c8d
Manyl Benabed <m_benabe@live.concordia.ca>
2026-09-10 11:18:48 -0400

    Second version: cleaner code and store tasks in json file

--- Main.py
+++ Main.py
@@ -1,82 +1,105 @@
-#code that is repeated can be simplified to a function that will be called when needed for making the program easier to read
-import sys
-
+import json
+from pathlib import Path
 tasks = []
-nb_of_actions = 3
+
+with open(Path("tasks.json"), "r") as file:
+    tasks = json.load(file)
+
+
+nb_of_actions = 5
+running = True
 
 def display_menu():
     print("""
-=========================
+        MENU
+====================
 
 1. Add Task
 2. View Tasks
 3. Remove Task
+4. Complete task
+5. Exit
 
-=========================
+====================
 """)
 
 
-def add_task(new_task):
+def add_task(new_task):  #deal with empt string as a task
     tasks.append(new_task)
+    print(f"'{new_task}' was successfully added")
 
-def remove_task(new_task):
+def remove_task(new_task):  #deal with empt string as a task
     found = False
     for task in tasks:
         if task == new_task:
             tasks.remove(task)
             found = True
             break
-
     if found == True:
-        print("Task removed successfully!")
+        print(f"'{new_task}' was removed successfully!")
     else:
-        print("Task not found.")
+        print(f"'{new_task}' was not found.")
 
+def view_tasks():
 
-while True: 
+    print("Your tasks : ")
+
+    for index, task in enumerate(tasks, start=1):
+        if task["completed"] == True:
+            print(f"{index}. [✓]  {task['task']}")
+
+        elif task["completed"] == False:
+            print(f"{index}. [ ]  {task['task']}")
+
+    print(f"Nb of tasks : {len(tasks)}")
+
+def complete_task():
+    view_tasks()
+    task_to_complete = input("Choose a task to complete from the list of tasks : ")
+    for task in tasks:
+        if task["completed"] == False and task_to_complete == task["task"]:
+            task["completed"] = True
+            print(f"'{task_to_complete}' has been successfully marked as completed")
+            with open(Path("tasks.json"), "w") as file:
+                json.dump(tasks, file)
+            
+            
+
+while running: 
     display_menu() 
+
     try:
-        action = int(input("Choose a number representing the action u would like to perform from the menu (1-3) : "))
+        action = int(input(f"Choose a number representing the action u would like to perform from the menu (1-{nb_of_actions}) : "))
 
     except ValueError as error:
-        print(f"{error} Try again")
+        print(f"{error} Try again. ")
         continue
 
     if not 1 <= action <= nb_of_actions:
-        print("You entered an invalid integer. Try again")
+        print("You entered an invalid integer. Try again.")
         continue
 
-    elif action == 1:    
-        new_task = input("What task do u wanna add : ")    #see different ways to handle text input from user, example accept is user enters Add task, add Task, Add Task, ... , here we cant use lower() or upper() so either use lst with tolerance writings, or some Python function exists or Ican create a loop that goes through all characters of theuser input and acts on first letter of every word
+    elif action == 1:   
+        new_task = input("What task do u wanna add?  : ") 
         add_task(new_task)
-        print(f"{new_task} was successfully added")
-        
 
     elif action == 2:
-        for index, task in enumerate(tasks, start=1):
-            print(index, task)
-        print(f"Nb of tasks : {len(tasks)}")
-        
+        view_tasks()    
 
     elif action == 3:
-        task = input("What task do u wanna remove : ")    
-        remove_task(task)
-        
+        new_task = input("What task do u wanna remove? : ")    
+        remove_task(new_task)
 
-    while True:
-        other_action = input("Do u wanna perform an other action? ").lower().strip()
+    elif action == 4:
+        complete_task()
 
-        if other_action == "yes":
-            break
-
-        elif other_action == "no":
-            print("Ok, goodbye")
-            sys.exit()
-
-        else:
-            print("You entered invalid input. Try again!")
-            continue    
-
+    elif action == 5:
+        print("Goodbye")
+        with open(Path("tasks.json"), "w") as file:
+            json.dump(tasks, file)
+        break
+                
+            
     
 
 
